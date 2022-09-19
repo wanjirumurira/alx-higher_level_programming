f9ee43cd90445aa3eb78ef8524d665e30696dcd6
wanjirumurira <wanjirumurira@gmail.com>
2022-09-19 13:29:40 -0700

    initial commit

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first3_letters = word[0:3]
+last2_letters = word[-2:]
+middle_word = word[1:8]
+print("First 3 letters: {}".format(first3_letters))
+print("Last 2 letters: {}".format(last2_letters))
+print("Middle word: {}".format(middle_word))
