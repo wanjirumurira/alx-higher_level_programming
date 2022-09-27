d0f0e464bb4618f8df770745ecf210b95a46ecd2
wanjirumurira <wanjirumurira@gmail.com>
2022-09-26 19:45:28 -0700

    adding line of code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) < idx < 0:
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
         return None
     else:
         return my_list[idx]
