6f94a8cfee483843d73e6b957bc25b209630ab12
wanjirumurira <wanjirumurira@gmail.com>
2022-09-26 19:39:05 -0700

    removing line of code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
 def element_at(my_list, idx):
-    for index in my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    if len(my_list) < idx < 0:
+        return None
+    else:
+        return my_list[idx]
