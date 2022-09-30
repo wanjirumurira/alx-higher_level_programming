da2d11f89cd296fe05d511bf97dc72e8cf505352
wanjirumurira <wanjirumurira@gmail.com>
2022-09-30 04:52:20 -0700

    adding a statement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
+    if not (isinstance(roman_string, str)):
+        return (0)
     for i in range(len(roman_string)):
         if (i+1 < len(roman_string) and
                 roman[roman_string[i]] < roman[roman_string[i+1]]):
