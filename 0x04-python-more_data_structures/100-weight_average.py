8a041a20de559322c85856a99de1311fa8a38b33
wanjirumurira <wanjirumurira@gmail.com>
2022-09-30 05:01:58 -0700

    initial commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
     sum = 0
-    y_total = 0
+    y_total = 0 
     for x, y in my_list:
-        if len(my_list) <= 0:
-            return 0
         sum += (x*y)
         y_total += y
     return (sum/y_total)
