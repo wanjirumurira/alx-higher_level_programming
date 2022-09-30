5ee117f3ff7ad1940ee1ae4631ab95969a7772de
wanjirumurira <wanjirumurira@gmail.com>
2022-09-30 03:23:45 -0700

    initial commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        if (i+1 < len(roman_string) and
+                roman[roman_string[i]] < roman[roman_string[i+1]]):
+            result -= roman[roman_string[i]]
+        else:
+            result += roman[roman_string[i]]
+    return result
