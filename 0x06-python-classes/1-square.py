e05fa451f3e310e4c40019f1849913bbc1f95932
wanjirumurira <wanjirumurira@gmail.com>
2022-10-12 08:09:06 -0700

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Define a class Square."""
+
+    def __init__(self, size):
+        """ Initialize method.
+        Args:
+            self (class): This class
+            size (int): Size of the square
+        """
+
+        """ Instance attribute
+        """
+        self.__size = size
